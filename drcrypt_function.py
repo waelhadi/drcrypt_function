c2e646e9e1bdd603d98ed9d6266240bd9b36c17f
waelhadi <56735070+waelhadi@users.noreply.github.com>
2024-11-15 04:36:36 +0100

    Update drcrypt_function.py

--- drcrypt_function.py
+++ drcrypt_function.py
@@ -1,8 +1,9 @@
 import base64, requests
 
 def fetch_key():
-
-	encrypted_key = "MTIzNDU2"  
+    # المفتاح المشفر
+    encrypted_key = "MTIzNDU2"  # المفتاح مشفر هنا باستخدام base64
+    # فك تشفير المفتاح عند الحاجة
     key = int(base64.b64decode(encrypted_key).decode())
     return key
 
@@ -11,16 +12,17 @@ def xor_decrypt(data, key):
 
 def decrypt_function(encrypted_parts):
     parts = encrypted_parts
-    key = fetch_key()   
+    key = fetch_key()  # جلب المفتاح وفك تشفيره عند الحاجة
 
-    for layer in range(3, 0, -1):   
+    # فك التشفير عبر الطبقات
+    for layer in range(3, 0, -1):  # بدءاً من الطبقة الأخيرة إلى الطبقة الأولى
         decrypted_parts_layer = []
 
         for part in parts:
-            decoded_part = base64.b64decode(part).decode('utf-8')  
+            decoded_part = base64.b64decode(part).decode('utf-8')  # فك التشفير باستخدام utf-8
             decrypted_part = xor_decrypt(decoded_part, key)
             decrypted_parts_layer.append(decrypted_part)
 
         parts = decrypted_parts_layer
 
-    return ''.join(parts)   
+    return ''.join(parts)  # إرجاع الكود الأصلي كنص واحد
